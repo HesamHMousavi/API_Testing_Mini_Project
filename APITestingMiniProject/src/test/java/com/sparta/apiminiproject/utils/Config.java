5e81e6d634a13ccb48a4fa0096d53ec143cc1b44
Hesam Mousavi <hm.mousavi.02@gmail.com>
2025-08-06 11:24:34 +0100

    Added base_uri getter

--- APITestingMiniProject/src/test/java/com/sparta/apiminiproject/utils/Config.java
+++ APITestingMiniProject/src/test/java/com/sparta/apiminiproject/utils/Config.java
@@ -5,27 +5,23 @@ import java.io.InputStream;
 import java.util.Properties;
 
 public class Config {
-//    private static final Properties properties = new Properties();
-//
-//    static {
-//        try (InputStream stream = Config.class
-//                .getClassLoader()
-//                .getResourceAsStream("config.properties")) {
-//            if (stream != null) {
-//                properties.load(stream);
-//            } else {
-//                throw new IOException("Unable to find config.properties");
-//            }
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
-//
-//    public static String getBaseUri() {
-//        return properties.getProperty("github.basr_uri");
-//    }
-//
-//    public static String getOwner() {
-//        return properties.getProperty("github.owner");
-//    }
+    private static final Properties properties = new Properties();
+
+    static {
+        try (InputStream stream = Config.class
+                .getClassLoader()
+                .getResourceAsStream("config.properties")) {
+            if (stream != null) {
+                properties.load(stream);
+            } else {
+                throw new IOException("Unable to find config.properties");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String getBaseUri() {
+        return properties.getProperty("base_uri");
+    }
 }
--- APITestingMiniProject/src/test/java/com/sparta/apiminiproject/utils/Utils.java
+++ APITestingMiniProject/src/test/java/com/sparta/apiminiproject/utils/Utils.java
@@ -1,5 +1,9 @@
 package com.sparta.apiminiproject.utils;
 
 public class Utils {
+    private static final String BASE_URI = Config.getBaseUri();
 
+    public String getBaseUri() {
+        return  BASE_URI;
+    }
 }
